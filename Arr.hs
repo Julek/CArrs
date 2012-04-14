cda1045c5daebcace1022f951bc81135b78af080
Julian Sutherland <julian.sutherland10@imperial.ac.uk>
2012-04-14 16:57:39 +0100

    reorg

--- Arr.hs
+++ Arr.hs
@@ -1,4 +1,4 @@
-module Arr (Arr, arrWrite, arrRead, arrCreate, arrSize) where
+module Arr (Arr, arrCreate, arrWrite, arrRead, arrSize) where
 
 import Control.Arrow
 import Foreign.C.Types
@@ -14,14 +14,14 @@ foreign import ccall "arrWrite" write :: CInt -> CInt -> CInt -> CInt
 foreign import ccall "arrRead" read :: CInt -> CInt -> CInt
 foreign import ccall "arrSize" size :: CInt -> CInt
 
+arrCreate :: IO Arr
+arrCreate = create >>= return . Arr
+
 arrWrite :: Arr -> Int -> Int -> Arr
 arrWrite = ((Arr .) .) . flip flip fromIntegral . ((.) .) . (. fromIntegral) . write . getArrId
 
 arrRead :: Arr -> Int -> Int
 arrRead = curry $ fromIntegral . uncurry read . (getArrId *** fromIntegral) 
-
-arrCreate :: IO Arr
-arrCreate = create >>= return . Arr
 
 arrSize :: Arr -> Int
 arrSize = fromIntegral . size . fromIntegral . getArrId
